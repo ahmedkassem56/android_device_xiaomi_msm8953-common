96d4f1bdf534ae3e020fc695d2b93c1b858f6086
ahmed_kassem56 <ahmed_kassem56@gmail.com>
2019-05-18 20:48:22 +0000

    msm8953-common: gps: Remove BUILD_TINY_ANDROID check
    Change-Id: Ie834f59dc6579eec307aabc93008cb505a52d923

--- gps/pla/Android.mk
+++ gps/pla/Android.mk
@@ -17,7 +17,6 @@ GNSS_CFLAGS := \
     -Wno-error=date-time
 
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
 
 LOCAL_PATH := $(call my-dir)
 
@@ -26,5 +25,4 @@ LOCAL_MODULE := libloc_pla_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/android
 include $(BUILD_HEADER_LIBRARY)
 
-endif # not BUILD_TINY_ANDROID
 endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
--- gps/utils/Android.mk
+++ gps/utils/Android.mk
@@ -1,5 +1,4 @@
 ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
-ifneq ($(BUILD_TINY_ANDROID),true)
 #Compile this library only for builds with the latest modem image
 
 LOCAL_PATH := $(call my-dir)
@@ -60,5 +59,4 @@ LOCAL_MODULE := libgps.utils_headers
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 include $(BUILD_HEADER_LIBRARY)
 
-endif # not BUILD_TINY_ANDROID
 endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
